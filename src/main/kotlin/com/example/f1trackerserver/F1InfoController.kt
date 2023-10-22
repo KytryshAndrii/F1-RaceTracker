a37646769d5e418bd8ff87c755644f8cc5c4d672
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-10-22 18:08:39 +0200

    Added file Structure to project. Added more pathes for info about results

---
+++ src/main/kotlin/com/example/f1trackerserver/F1InfoController.kt
@@ -0,0 +1,47 @@
+package com.example.f1trackerserver
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
+
+@RestController
+@RequestMapping("/f1/info")
+class F1InfoController {
+    @GetMapping("/drivers")
+    fun getF1Drivers():String{
+        val restTemplate = RestTemplate()
+        val url = "https://ergast.com/api/f1/drivers.json?limit=859"
+        return restTemplate.getForObject(url, String::class.java) ?: "No data received"
+    }
+
+
+    @GetMapping("/rounds")
+    fun getF1RoundsByYear(@RequestParam year: String): Int?{
+        val restTemplate = RestTemplate()
+        val url: String = "https://ergast.com/api/f1/$year.json"
+        val result: String = restTemplate.getForObject(url, String::class.java) ?: "No data received"
+        val count: Int = StringEdditor().countMatches(result, "\"round\"")
+        return count
+    }
+
+    @GetMapping("/constructors")
+    fun getF1ConstruktorsByDriver(@RequestParam driver: String): String?{
+        val restTemplate = RestTemplate()
+        val url: String = "https://ergast.com/api/f1/drivers/$driver/constructors.json"
+        val response: String = restTemplate.getForObject(url, String::class.java) ?: "No data received"
+        val result: String = StringEdditor().cutRightPart(response, "\"driverId\":\"$driver\",")
+        return result.dropLast(2)
+    }
+
+    @GetMapping("/bydateconstructors")
+    fun getF1ConstruktorsByDate(@RequestParam year: String): String?{
+        val restTemplate = RestTemplate()
+        val url: String = "http://ergast.com/api/f1/$year/constructors.json"
+        val response: String = restTemplate.getForObject(url, String::class.java) ?: "No data received"
+        val result: String = StringEdditor().cutRightPart(response, "\"Constructors\":")
+        return  result.dropLast(2).replaceFirstChar { "{Constructors:" }
+    }
+
+}
--- src/main/kotlin/com/example/f1trackerserver/F1TrackerServerApplication.kt
+++ src/main/kotlin/com/example/f1trackerserver/F1TrackerServerApplication.kt
@@ -2,47 +2,10 @@ package com.example.f1trackerserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.client.RestTemplate
-
 
 @SpringBootApplication
 class F1TrackerServerApplication
 
 fun main(args: Array<String>) {
     runApplication<F1TrackerServerApplication>(*args)
-}
-@RestController
-@RequestMapping("/f1")
-class F1ResultsController {
-    @GetMapping("/results")
-    fun getF1Results(): String {
-        val restTemplate = RestTemplate()
-        val url = "http://ergast.com/api/f1/current/last/results.json"
-        return restTemplate.getForObject(url, String::class.java) ?: "No data received"
-    }
-
-    @GetMapping("/drivers")
-        fun getF1Drivers():String{
-        val restTemplate = RestTemplate()
-        val url = "https://ergast.com/api/f1/2008/drivers.json"
-        return restTemplate.getForObject(url, String::class.java) ?: "No data received"
-        }
-
-
-    @GetMapping("/rounds")
-    fun getF1RoundsByYear(@RequestParam year: String): String?{
-        val restTemplate = RestTemplate()
-        val url = "https://ergast.com/api/f1/$year.json"
-        val result = restTemplate.getForObject(url, String::class.java) ?: "No data received"
-        val regex = Regex("round (.*?)[.]")
-        val matches = regex.findAll(result)
-        val names = matches.map { it.groupValues[1] }.joinToString()
-        println(names)
-        return result
-    }
-
 }
