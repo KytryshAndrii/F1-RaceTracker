77f9b85053582f2479fc1d8e8cf4e2c33fde089c
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-10-31 19:22:13 +0100

    Basic select panel functionality done. NEXT -> STARTED WRITING ResultAPI

--- f1trackerfrontend/src/API/InfoAPI.tsx
+++ f1trackerfrontend/src/API/InfoAPI.tsx
@@ -1,7 +1,7 @@
 export default class  F1InfoAPI{
 
-     async Drivers(url: string): Promise<any[]> {
-        const result = fetch(url).then(response => {
+     async Drivers(): Promise<any[]> {
+        const result = fetch("http://localhost:8080/f1/info/drivers").then(response => {
                 if (!response.ok) {
                     throw new Error(response.statusText)
                 }
@@ -18,5 +18,79 @@ export default class  F1InfoAPI{
         return listtoreturn
 
     }
+
+    async Rounds(year: string): Promise<number>{
+         const result = fetch("http://localhost:8080/f1/info/rounds?year=" + year).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json().then((value) => {
+                return value;
+            })
+        })
+        var toreturn: number = 0;
+        result.then((resultvalue) =>{
+            setTimeout(()=>{
+                toreturn = resultvalue
+            }, 500)
+        })
+        return toreturn
+    }
+
+    async Constructor(driver: string): Promise<any[]> {
+        const result = fetch("http://localhost:8080/f1/info/constructors?driver="+ driver).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json().then((value) => {
+                return value.Constructors;
+            })
+        })
+        var listtoreturn: any[]= []
+        result.then((resultvalue) =>{
+            setTimeout(()=>{resultvalue.map((elem: any)=> {
+                listtoreturn.push({value:elem.constructorId, label:elem.name , nation: elem.nationality})
+            });}, 500)
+        })
+        return listtoreturn
+
+    }
+
+    async ConstructorByDate(year: number): Promise<any[]> {
+        const result = fetch("http://localhost:8080/f1/info/bydateconstructors?year="+ year).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json().then((value) => {
+                return value.Constructors;
+            })
+        })
+        var listtoreturn: any[]= []
+        result.then((resultvalue) =>{
+            setTimeout(()=>{resultvalue.map((elem: any)=> {
+                listtoreturn.push({value:elem.constructorId, label:elem.name , nation: elem.nationality})
+            });}, 500)
+        })
+        return listtoreturn
+
+    }
+
+    async YearsList(): Promise<any[]>{
+        var listtoreturn: any[] = []
+        const result = fetch("http://localhost:8080/f1/info/yearlist").then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json()
+        })
+        result.then((resultvalue) =>{
+            setTimeout(()=>{resultvalue.map((elem: any)=> {
+                listtoreturn.push({value:elem, label:elem.toString().toLocaleUpperCase()})
+            });}, 500)
+        })
+        return listtoreturn
+
+    }
+
 
 };
--- f1trackerfrontend/src/API/ResultsAPI.tsx
+++ f1trackerfrontend/src/API/ResultsAPI.tsx
@@ -1,13 +1,31 @@
 export default class  ResultsAPI{
-
-    async CheckProxy() {
-        return fetch("f1/connection/connect")
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.statusText)
-                }
-                //console.log(response.text())
-                return response
+    async Drivers(): Promise<any[]> {
+        const result = fetch("http://ergast.com/api/f1/current/last/results.json").then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText)
+            }
+            return response.json().then((value) => {
+                return value.MRData.RaceTable.Races;
             })
+        })
+        var listtoreturn: any[]= []
+        var season: string = ""
+        var location: string = ""
+        var date: string = ""
+        var time: string = ""
+        result.then((resultvalue) =>{
+            season = resultvalue.season
+            location = resultvalue.Circuit.Location.locality + " " + resultvalue.Circuit.Location.country
+            date = resultvalue.date
+            time = resultvalue.time
+            setTimeout(()=>{resultvalue.Results.map((elem: any)=> {
+                listtoreturn.push({number: elem.number, position: elem.position, points: elem.points,
+                 driver: elem.Driver.givenName + " " + elem.Driver.familyName,
+                })})
+                listtoreturn.push({season: season, location: location, date: date, time: time})
+            ;}, 500)
+        })
+        return listtoreturn
+
     }
 };
