01390f2c881c53d3d7aab62d98fc7b3e115cc699
Kytrysh Andrii <63211791+KytryshAndrii@users.noreply.github.com>
2023-11-05 23:40:10 +0100

    Added recent results. Loading Bar, Routing on pages. Api processing

--- f1trackerfrontend/src/API/ResultsAPI.tsx
+++ f1trackerfrontend/src/API/ResultsAPI.tsx
@@ -1,11 +1,11 @@
 export default class  ResultsAPI{
-    async Drivers(): Promise<any[]> {
+    async Recent(): Promise<any[]> {
         const result = fetch("http://ergast.com/api/f1/current/last/results.json").then(response => {
             if (!response.ok) {
                 throw new Error(response.statusText)
             }
             return response.json().then((value) => {
-                return value.MRData.RaceTable.Races;
+                return value.MRData.RaceTable.Races[0];
             })
         })
         var listtoreturn: any[]= []
@@ -15,13 +15,25 @@ export default class  ResultsAPI{
         var time: string = ""
         result.then((resultvalue) =>{
             season = resultvalue.season
-            location = resultvalue.Circuit.Location.locality + " " + resultvalue.Circuit.Location.country
+            location = resultvalue.Circuit.Location.locality + " - " + resultvalue.Circuit.Location.country
             date = resultvalue.date
-            time = resultvalue.time
+            time = resultvalue.time.slice(0, -1)
             setTimeout(()=>{resultvalue.Results.map((elem: any)=> {
-                listtoreturn.push({number: elem.number, position: elem.position, points: elem.points,
-                 driver: elem.Driver.givenName + " " + elem.Driver.familyName,
-                })})
+                if(elem.FastestLap){
+                    listtoreturn.push(
+                        {number: elem.number, position: elem.position, points: elem.points,
+                         driver: elem.Driver.givenName + " " + elem.Driver.familyName, constructor: elem.Constructor.name,
+                         status: elem.status, url: elem.Driver.url,
+                         fastesttime: elem.FastestLap.Time.time, averspeed: elem.FastestLap.AverageSpeed.speed
+                        })
+                }else{
+                    listtoreturn.push(
+                        {number: elem.number, position: elem.position, points: elem.points,
+                         driver: elem.Driver.givenName + " " + elem.Driver.familyName, constructor: elem.Constructor.name,
+                         status: elem.status, url: elem.Driver.url
+                        })
+                }
+            })
                 listtoreturn.push({season: season, location: location, date: date, time: time})
             ;}, 500)
         })
